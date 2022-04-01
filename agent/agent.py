73250678b1878cbb307b63bfbdee72620b009f03
chevalier3as <chevalier3as@gmail.com>
2022-04-01 16:39:10 +0000

    Add ability to inject multiple assets to inject asset. This is a breaking changes from previous agents.

--- agent/agent.py
+++ agent/agent.py
@@ -1,18 +1,19 @@
 """Sample agent implementation"""
+import glob
 import logging
 
 from ostorlab.agent import agent
 from rich import logging as rich_logging
 
-ASSET_RAW_PATH = '/tmp/asset.binproto'
-ASSET_SELECTOR_PATH = '/tmp/asset_selector.txt'
+ASSET_DIR = '/asset/'
 
+RAW_PATTERN = 'asset.binproto_'
+SELECTOR_PATTERN = '/asset/selector.txt_'
 
 logging.basicConfig(
     format='%(message)s',
     datefmt='[%X]',
     handlers=[rich_logging.RichHandler(rich_tracebacks=True)],
-    level='INFO',
     force=True
 )
 logger = logging.getLogger(__name__)
@@ -23,12 +24,15 @@ class AgentInjectAsset(agent.Agent):
 
     def start(self) -> None:
         try:
-            with open(ASSET_RAW_PATH, 'rb') as asset_raw_o, open(ASSET_SELECTOR_PATH, 'r',
-                                                                 encoding='utf-8') as asset_selector_o:
-                asset = asset_raw_o.read()
-                selector = asset_selector_o.read()
-                logger.info('injecting asset of size %d to selector %s', len(asset), selector)
-                self.emit_raw(selector=selector, raw=asset)
+            for raw_asset_path in glob.glob(f'{ASSET_DIR}{RAW_PATTERN}*'):
+                asset_id = raw_asset_path.split('_', 1)[1]
+                asset_selector_path = f'{SELECTOR_PATTERN}{asset_id}'
+                with open(raw_asset_path, 'rb') as asset_raw_o, open(
+                        asset_selector_path, 'r', encoding='utf-8') as asset_selector_o:
+                    asset = asset_raw_o.read()
+                    selector = asset_selector_o.read()
+                    logger.info('injecting asset of size %d to selector %s', len(asset), selector)
+                    self.emit_raw(selector=selector, raw=asset)
         except FileNotFoundError as e:
             logger.error('expected asset files are not found: %s', e)
             raise
