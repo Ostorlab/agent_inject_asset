5c215af1c438bc7078f3829b68cc7199ec37b297
chevalier3as <chevalier3as@gmail.com>
2022-04-02 07:47:10 +0000

    Add backward compatible implementation to avoid breaking users.

--- agent/agent.py
+++ agent/agent.py
@@ -1,14 +1,18 @@
 """Sample agent implementation"""
 import glob
 import logging
+import pathlib
 
 from ostorlab.agent import agent
 from rich import logging as rich_logging
 
 ASSET_DIR = '/asset/'
-
 RAW_PATTERN = 'asset.binproto_'
 SELECTOR_PATTERN = '/asset/selector.txt_'
+
+# Before Ostorlab version 0.3.1 (including).
+ASSET_RAW_PATH = '/tmp/asset.binproto'
+ASSET_SELECTOR_PATH = '/tmp/asset_selector.txt'
 
 logging.basicConfig(
     format='%(message)s',
@@ -23,6 +27,26 @@ class AgentInjectAsset(agent.Agent):
     """Agent Inject Asset."""
 
     def start(self) -> None:
+        if pathlib.Path(ASSET_RAW_PATH).exists():
+            self._legacy_emit_assets()
+        else:
+            self._emit_assets()
+
+    def _legacy_emit_assets(self):
+        """Asset injection as done by Ostorlab version 0.3.1 and before."""
+        try:
+            with open(ASSET_RAW_PATH, 'rb') as asset_raw_o, open(ASSET_SELECTOR_PATH, 'r',
+                                                                 encoding='utf-8') as asset_selector_o:
+                asset = asset_raw_o.read()
+                selector = asset_selector_o.read()
+                logger.info('injecting asset of size %d to selector %s', len(asset), selector)
+                self.emit_raw(selector=selector, raw=asset)
+        except FileNotFoundError as e:
+            logger.error('expected asset files are not found: %s', e)
+            raise
+
+    def _emit_assets(self):
+        """Asset injection as done in all new versions."""
         try:
             for raw_asset_path in glob.glob(f'{ASSET_DIR}{RAW_PATTERN}*'):
                 asset_id = raw_asset_path.split('_', 1)[1]
--- tests/agent_test.py
+++ tests/agent_test.py
@@ -55,3 +55,25 @@ def testInjectAssetAgent_withMultipleAsset_rawAssetAreInjected(agent_mock, fs):
     assert agent_mock[0].raw == msg.raw
     assert agent_mock[1].selector == 'v3.asset.file.android.apk'
     assert agent_mock[1].raw == msg.raw
+
+def testInjectAssetAgent_whenLegacyAssetInjection_rawAssetIsInjected(agent_mock, fs):
+    """Ensures file is injected using the provided selector."""
+    # The pymockfs overrides the whole filesystem, which causes the message serialization to fail as it is lookgin for
+    # proto files. This add a passthrough to the real filesystem.
+    fs.add_real_directory('/home/')
+    fs.add_real_directory('/opt/')
+
+    msg = message.Message.from_data(selector='v3.asset.file.android.apk', data={'content': b'FAKE'})
+    fs.create_file(file_path=agent.ASSET_RAW_PATH, contents=msg.raw)
+    fs.create_file(file_path=agent.ASSET_SELECTOR_PATH, contents='v3.asset.file.android.apk')
+
+    definition = agent_definitions.AgentDefinition(
+        name='start_test_agent',
+        out_selectors=['v3.asset.file.android.apk'])
+    settings = runtime_definitions.AgentSettings(key='agent/ostorlab/agent_inject_asset')
+
+    test_agent = agent.AgentInjectAsset(definition, settings)
+    test_agent.run()
+    assert len(agent_mock) == 1
+    assert agent_mock[0].selector == 'v3.asset.file.android.apk'
+    assert agent_mock[0].raw == msg.raw
